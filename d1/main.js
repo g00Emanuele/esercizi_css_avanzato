5eb9e34d006d43f56459205648fcfe99a19eb118
g00Emanuele <g00emanuele@gmail.com>
2023-06-21 21:03:27 +0200

    Correzione es2 js

--- d1/main.js
+++ d1/main.js
@@ -1,13 +1,22 @@
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il link "twitter" sotto alla sezione "Altro" nell'elemento "aside". Deve avvenire al caricamento della pagina, automticamente.
 
-let twit = document.getElementsByTagName('a')
+// Primo modo
+// let link = document.getElementsByTagName('a')
 
-for (let i = 0; i < twit.length ; i++){
-    let linkCorrente = twit[i]
-    if(linkCorrente.textContent === 'Twitter'){
-        linkCorrente.remove()
-    }
-}
+// for (let i = 0; i < link.length ; i++){
+//     let linkCorrente = link[i]
+//     if(linkCorrente.textContent === 'Twitter'){
+//         linkCorrente.remove()
+//     }
+// }
+
+// Secondo modo
+let twit = document.querySelector('.p-4:nth-child(3) .list-unstyled li:nth-child(2) a')
+
+window.addEventListener("load", (event) => {
+    twit.remove()
+})
+
 
 // EXTRA Crea con JavaScript la funzionalità per rimuovere il corrispondente elemento padre dal DOM cliccando sul link "Continua a leggere".
 
